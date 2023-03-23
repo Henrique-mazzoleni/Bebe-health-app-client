2a4c563611cc96950e74dd94a76f1b8e6b8ccdbc
Henrique Mazzoleni <henrique.mazzoleni@gmail.com>
2023-03-23 17:54:44 -0300

    added invite route and finished page

--- src/pages/Invite.jsx
+++ src/pages/Invite.jsx
@@ -1,47 +1,88 @@
-import React from 'react'
-import { Form } from 'react-bootstrap'
+import React, { useEffect, useState } from "react";
+import Form from "react-bootstrap/Form";
+import { Alert, Button } from "react-bootstrap";
+import { useNavigate } from "react-router-dom";
+import axios from "axios";
+
+const API_URL = "http://localhost:5005";
 
 function Invite() {
+  const [email, setEmail] = useState();
+  const [childId, setChildId] = useState();
+  const [user, setUser] = useState();
+  const [error, setError] = useState("");
 
-    const [email, setEmail] = useState();
-    const [child, setChild] = useState();
+  const navigate = useNavigate();
 
+  const storedToken = localStorage.getItem("authToken");
 
-    const emailHandler = (e) => setEmail(e.target.value);
-    const childHandler = (e) => setChild(e.target.value);
+  const emailHandler = (e) => setEmail(e.target.value);
+  const childHandler = (e) => setChildId(e.target.value);
 
+  useEffect(() => {
+    axios
+      .get("http://localhost:5005/api/parent", {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        setUser(response.data);
+      });
+  }, []);
+
+  const submitHandler = async (e) => {
+    e.preventDefault();
+
+    try {
+      const invite = await axios.post(
+        `${API_URL}/api/parent/invite`,
+        { emailToInvite: email, childId },
+        {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        }
+      );
+
+      navigate("/profile");
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
 
   return (
     <>
-    <div><h1>Invite</h1>
-    
-    <Form onSubmit={submitHandler}>
-        <Form.Group className="mb-3" controlId="formGroupEmail">
-          <Form.Label>Email to invite</Form.Label>
-          <Form.Control
-            type="email"
-            placeholder="Enter other gaurdians email"
-            onChange={emailHandler}
-            value={email}
-          />
-        </Form.Group>
-        <Form.Group className="mb-3" controlId="formGroupChild">
-          <Form.Label>Children</Form.Label>
-          <Form.Select aria-label="children">
-          
-      <option>Open this select menu</option>
-      <option value="1">One</option>
-      <option value="2">Two</option>
-      <option value="3">Three</option>
-    </Form.Select>
-        </Form.Group>
+      <div>
+        <h1>Invite</h1>
+
+        <Form onSubmit={submitHandler}>
+          <Form.Group className="mb-3" controlId="formGroupEmail">
+            <Form.Label>Email to invite</Form.Label>
+            <Form.Control
+              type="email"
+              placeholder="Enter other gaurdians email"
+              onChange={emailHandler}
+              value={email}
+            />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formGroupChild">
+            <Form.Label>Children</Form.Label>
+            <Form.Select aria-label="children" onChange={childHandler}>
+              <option>Choose a Child</option>
+              {user?.children.map((child) => (
+                <option value={child._id}>{child.name}</option>
+              ))}
+            </Form.Select>
+          </Form.Group>
+          <Button type="submit" variant="primary">
+            Submit
+          </Button>
+          {error && (
+            <Alert key="danger" variant="danger">
+              {error}
+            </Alert>
+          )}
         </Form>
-    
-    </div>
-
-
+      </div>
     </>
-  )
+  );
 }
 
-export default Invite
+export default Invite;
--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -1,54 +1,87 @@
-import axios from 'axios'
-import React, { useEffect, useState } from 'react'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
-import Sleep from '../assets/images/sleep.jpg'
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+import Button from "react-bootstrap/Button";
+import Card from "react-bootstrap/Card";
+import Sleep from "../assets/images/sleep.jpg";
+import { useNavigate } from "react-router-dom";
+
+const API_URL = "http://localhost:5005";
 
 function Profile() {
+  const [user, setUser] = useState();
 
-  
+  const storedToken = localStorage.getItem("authToken");
 
-  const [user, setUser] = useState()
+  const navigate = useNavigate();
 
-  const storedToken = localStorage.getItem("authToken")
-  useEffect(()=>{axios.get("http://localhost:5005/api/parent",
-  { headers: { Authorization: `Bearer ${storedToken}` } })
-  .then((response) => {
-    // setUser(response.data)
-    setUser(response.data)
-  })},[])
+  console.log(user);
+  useEffect(() => {
+    axios
+      .get("http://localhost:5005/api/parent", {
+        headers: { Authorization: `Bearer ${storedToken}` },
+      })
+      .then((response) => {
+        setUser(response.data);
+      });
+  }, []);
 
-  
- 
+  const acceptInviteHandler = (inviteId) => {
+    axios
+      .post(
+        `${API_URL}/api/parent/accept`,
+        { inviteId },
+        {
+          headers: { Authorization: `Bearer ${storedToken}` },
+        }
+      )
+      .then((response) => {
+        const child = response.data;
 
-    
-    
+        navigate(`/child/${child._id}`);
+      });
+  };
+
   return (
     <div>
-    <h1>{user?.name}</h1>
-    <div className='homeCards'>
-    {user?.children.map((singleChild)=>{
-      return(
+      <h1>{user?.name}</h1>
+      <div className="homeCards">
+        {user?.children.map((singleChild) => {
+          return (
+            <Card style={{ width: "18rem" }}>
+              <Card.Img variant="top" src={Sleep} />
+              <Card.Body>
+                <Card.Title>{singleChild.name}</Card.Title>
+                <Card.Text>{singleChild.dateOfBirth}</Card.Text>
+                <Button href={`child/${singleChild._id}`} variant="primary">
+                  Go to {singleChild.name}
+                </Button>
+              </Card.Body>
+            </Card>
+          );
+        })}
+      </div>
 
-        <Card style={{ width: '18rem' }}>
-          <Card.Img variant="top" src={Sleep} />
-          <Card.Body>
-            <Card.Title>{singleChild.name}</Card.Title>
-            <Card.Text>
-            {singleChild.dateOfBirth}
-            </Card.Text>
-            <Button href={`child/${singleChild._id}`} variant="primary">Go to {singleChild.name}</Button>
-          </Card.Body>
-        </Card>
-        )
-    })}
+      <a href="/newchild">Add a Child</a>
+      <a href="/invite">Invite another Parent</a>
+
+      {user?.invitations.length !== 0 && <h3>Your Invitations</h3>}
+      {user?.invitations.map((invite) => {
+        return (
+          <Card style={{ width: "18rem" }}>
+            <Card.Body>
+              <Card.Title>{invite.childToAdd.name}</Card.Title>
+              <Card.Text>invite from {invite.invitationFrom.name}</Card.Text>
+              <Button
+                onClick={acceptInviteHandler.bind(null, invite._id)}
+                variant="primary">
+                Accept
+              </Button>
+            </Card.Body>
+          </Card>
+        );
+      })}
     </div>
-    
-    
-   
-    <a href='/newchild'>Add a Child</a>
-    </div>
-  )
+  );
 }
 
-export default Profile
+export default Profile;
