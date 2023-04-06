b06daf986654bdf62e52e35332b77b2c676039b2
Craig Butler <craig.butler@jrbsolutions.co.uk>
2023-04-06 14:19:11 +0100

    removing loading animation test

--- src/components/isAnon.jsx
+++ src/components/isAnon.jsx
@@ -1,7 +1,6 @@
 import { useContext } from "react";
 import { AuthContext } from "../context/auth.context";
 import { Navigate } from "react-router-dom";
-import Spinner from 'react-bootstrap/Spinner';
 
 export default function IsAnon({ children }) {
   const { isLoggedIn, isLoading } = useContext(AuthContext);
